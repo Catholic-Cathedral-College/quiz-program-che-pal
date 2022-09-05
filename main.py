44347337f0a72b2a38c4d1a56fab314d0a16325d
CheyanneP <112838731+che-pal@users.noreply.github.com>
2022-09-05 02:59:11 +0000

    Found the lost code!!!

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello World")
+from time import sleep
+#Intro
+print("*your eyes slowly open*")
+sleep(2)
+print('I see nothing')
+sleep(2)
+print("What is this?")
+sleep(2)
+print("What is happening.")
+sleep(2)
+print(
+    "*In the distance you hear sirens and blue and red lights piercing through the pitch darkness*"
+)
+sleep(4)
+print("Ah, I remember... I'm dying.")
+sleep(4)
+print(
+    'What a shame... I barely even "lived" my whole life and now here I am lying on this cold concrete floor covered in pool of blood'
+)
+sleep(2)
+print("Hmm...")
+sleep(1)
+print(
+    "Isnt this the part when im having flashbacks? Just like how I see in movies retracing every memory they have left until they took their last breath."
+)
+sleep(2)
+print("Why am I ")
